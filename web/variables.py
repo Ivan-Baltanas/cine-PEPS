583e1b74a01dccbf54065a03c6997b8a1ce9f03a
root <root@max11>
2025-01-24 12:35:45 +0100

    Prueba

--- web/variables.py
+++ web/variables.py
@@ -2,8 +2,9 @@ import os
 def cargarvariables():
     os.environ['DB_USERNAME']='root'
     os.environ['DB_PASSWORD']='example'
-    os.environ['DB_DATABASE']='PEPS'
+    os.environ['DB_DATABASE']='CineApp'
     os.environ['DB_HOST']='localhost'
     os.environ['DB_PORT']='3306'
     os.environ['PORT']='8080'
     os.environ['HOST']='0.0.0.0'
+    
