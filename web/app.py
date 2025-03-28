76dc401d51e3f83d7d556db2e260864f2188f11a
Ivan-Baltanas <ivan.baltanas-ortiz@iessampedro.org>
2025-03-28 12:19:22 +0100

    ejer 7 hecho

--- web/app.py
+++ web/app.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
+csrf = CSRFProtect(app)
+@app.before_request
+def csrf_protect():
+ if not request.path.startswith("/login") and not request.path.startswith("/registro"):
+ csrf.protect()
 
-app.config.from_pyfile('settings.py')  
 import rutas_inicio
 
 import rutas_upload
