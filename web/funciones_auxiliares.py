db8855bc3166b828c84170b2fdddbdd84feb847d
ciber-alberto <m0lina2tiktok@gmail.com>
2025-04-01 12:23:04 +0200

    Ej 1-5 hechos

--- web/funciones_auxiliares.py
+++ web/funciones_auxiliares.py
@@ -1,4 +1,16 @@
+from werkzeug.http import http_date
+from flask import session
+import json
+import decimal
+import html
+import bleach
 import bcrypt
+import datetime
+# import html
+# import bleach
+# import bcrypt
+# from flask import session
+
 
 def cipher_password(password):
   hashAndSalt = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(10))
@@ -11,3 +23,23 @@ def compare_password(password_hash,password):
       return bcrypt.checkpw(password,password_hash)
    except:
       return False
+   
+def sanitize_input(user_input):
+ # Usamos bleach para eliminar etiquetas HTML no deseadas
+ escaped_input = html.escape(user_input)
+ return bleach.clean(escaped_input)    
+    
+def create_session(usuario,perfil):
+    session["usuario"]=usuario
+    session["perfil"]=perfil
+def delete_session():
+    session.clear()
+
+def validar_session_normal():
+    try:
+        if (session["usuario"] and session["usuario"]!=""):
+            return True
+        else:
+            return False
+    except:
+        return False
