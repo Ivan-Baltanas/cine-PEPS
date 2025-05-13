e326710f5d0a06c5ceb4b1d936d9fc0b79aaf4f2
ciber-alberto <m0lina2tiktok@gmail.com>
2025-05-13 12:33:06 +0200

    Todo

--- web/controlador_usuarios.py
+++ web/controlador_usuarios.py
@@ -2,6 +2,7 @@ from bd import obtener_conexion
 from funciones_auxiliares import create_session, compare_password, cipher_password
 import datetime as dt
 from flask_wtf.csrf import generate_csrf, CSRFProtect
+from app import app
 
 def login_usuario(username, passwordIn):
     try:
